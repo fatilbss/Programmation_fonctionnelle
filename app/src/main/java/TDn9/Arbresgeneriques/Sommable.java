d0d0cbe2eb6edbd88867e65f47fcf44829935488
Fatimata Soumaré <fati.soumare@outlook.fr>
2021-10-18 16:52:45 +0200

    arbres generiques question 2

--- app/src/main/java/TDn9/Arbresgeneriques/Sommable.java
+++ app/src/main/java/TDn9/Arbresgeneriques/Sommable.java
@@ -4,3 +4,7 @@ public interface Sommable {
         T sommer(final T autre);
         T zero();
 }
+
+// définir une interface Sommable caractérisant les objets pouvant être sommés : ils ont
+//une opération sommer qui prend un objet du même type qu’eux et renvoit leur somme, qui est un
+//objet du même type.
